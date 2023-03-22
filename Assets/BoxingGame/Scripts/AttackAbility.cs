7bd95c969f1a030b2f172f0525d82eaad183d057
Avidys1 <69292284+Avidys1@users.noreply.github.com>
2023-03-22 18:01:38 +1100

    fix

---
+++ Assets/BoxingGame/Scripts/AttackAbility.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAbility : PlayerAbility
+{
+   public override void UseAbility()
+   {
+	 if(_turnTimer.IsNextTurn())
+	 {
+		 int damage = Random.Range(20, 30);
+		 //Debug.Log("ATTACK" + damage);
+		 _enemy.DealDamage(damage);
+		 EndTurn();
+	 }		 
+   }
+    
+}
